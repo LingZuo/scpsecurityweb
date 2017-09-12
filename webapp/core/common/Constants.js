79f1ef0aa2334bce70b4c4687ce621b6bd5e7da3
Ling Zuo <ling.zuo@sap.com>
2017-09-12 15:35:50 +0800

    Delete commit

--- webapp/core/common/Constants.js
+++
@@ -1,4 +0,0 @@
-jQuery.sap.declare("sap.security.core.common.Constants");
-sap.security.core.common.Constants = {
-	
-};
--- webapp/userroles/controller/NotFound.controller.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-	"sap/security/userroles/controller/BaseController"
-], function(BaseController) {
-	"use strict";
-
-	return BaseController.extend("sap.security.userroles.controller.NotFound", {
-
-	});
-});
